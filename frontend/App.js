1f09283060c4f1647a05fce3855a04466b9eb4ce
Rodesc <rodolphe.deschaetzen@gmail.com>
2023-10-30 22:57:08 +0100

    [ADD] assets: added ka-blam font

--- frontend/App.js
+++ frontend/App.js
@@ -1,16 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useCallback } from 'react';
+
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    'Ka-Blam': require('./assets/Ka-Blam.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Attention Voteuuuh</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={styles.title}>
+        ATTENTION VOTEUUUH
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  title: {
+    fontFamily: 'Ka-Blam',
+    fontSize: 30,
+    letterSpacing: 2,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
