dc0fd387bdab37134c5427674652f0b47831b741
Louis Verbiest <lve@idealisconsulting.com>
2023-11-05 22:26:05 +0100

    Add Navbar Components from react-navigation

--- frontend/App.js
+++ frontend/App.js
@@ -1,34 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native';
 import { useCallback } from 'react';
-
+import { Button, Text, View, StyleSheet } from 'react-native';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 SplashScreen.preventAutoHideAsync();
-
-export default function App() {
-  const [fontsLoaded, fontError] = useFonts({
-    'Ka-Blam': require('./assets/Ka-Blam.ttf'),
-  });
-
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded || fontError) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded, fontError]);
-
-  if (!fontsLoaded && !fontError) {
-    return null;
-  }
-
-  return (
-    <View style={styles.container} onLayout={onLayoutRootView}>
-      <Text style={styles.title}>
-        ATTENTION VOTEUUUH
-      </Text>
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
   title: {
@@ -43,3 +21,83 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+function DetailsScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Details!</Text>
+        </View>
+    );
+}
+
+function HomeScreen({ navigation }) {
+    const [fontsLoaded, fontError] = useFonts({
+        'Ka-Blam': require('./assets/Ka-Blam.ttf'),
+    });
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded || fontError) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded, fontError]);
+
+    if (!fontsLoaded && !fontError) {
+        return null;
+    }
+
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }} onLayout={onLayoutRootView}>
+            <Text style={styles.title}>BLUE THRILLS UNITED</Text>
+            <Button
+                title="Go to Details"
+                onPress={() => navigation.navigate('HomeDetails')}
+            />
+        </View>
+    );
+}
+
+function SettingsScreen({ navigation }) {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Settings screen</Text>
+            <Button
+                title="Go to Details"
+                onPress={() => navigation.navigate('SettingsDetails')}
+            />
+        </View>
+    );
+}
+
+const HomeStack = createNativeStackNavigator();
+
+function HomeStackScreen() {
+    return (
+        <HomeStack.Navigator>
+            <HomeStack.Screen name="Home" component={HomeScreen} />
+            <HomeStack.Screen name="HomeDetails" component={DetailsScreen} />
+        </HomeStack.Navigator>
+    );
+}
+
+const SettingsStack = createNativeStackNavigator();
+
+function SettingsStackScreen() {
+    return (
+        <SettingsStack.Navigator>
+            <SettingsStack.Screen name="Settings" component={SettingsScreen} />
+            <SettingsStack.Screen name="SettingsDetails" component={DetailsScreen} />
+        </SettingsStack.Navigator>
+    );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator screenOptions={{ headerShown: false }}>
+                <Tab.Screen name="Home" component={HomeStackScreen} />
+                <Tab.Screen name="Settings" component={SettingsStackScreen} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
